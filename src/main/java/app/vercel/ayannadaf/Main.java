d8e7de756d4e836c0d4eeb3503bd65ad6a2c0607
Ayan Nadaf <nadafayan95@gmail.com>
2026-06-24 19:20:35 +0530

    Created a Beans.xml file to Configure the IoC container

--- src/main/java/app/vercel/ayannadaf/Main.java
+++ src/main/java/app/vercel/ayannadaf/Main.java
@@ -1,17 +1,15 @@
 package app.vercel.ayannadaf;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
     static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+        ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
-        }
+        OrderService order = context.getBean(OrderService.class);
+
+        order.placeOrder();
     }
 }
+
---
+++ src/main/java/app/vercel/ayannadaf/OrderService.java
@@ -0,0 +1,11 @@
+package app.vercel.ayannadaf;
+
+public class OrderService {
+
+    public OrderService() {
+        System.out.println("Bean of OrderService.java created");
+    }
+    public void placeOrder() {
+        System.out.println("Order placed");
+    }
+}
